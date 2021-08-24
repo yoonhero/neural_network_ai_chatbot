7a2bdd1bb4b6f96ef8ec04e3d2920e1d33a2c0a0
yoonhero <yoonhero06@naver.com>
2021-08-25 08:27:58 +0900

    initial commit

---
+++ training.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pickle
+import json
+import random
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+import nltk
+from nltk.stem import WordNetLemmatizer
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# import ssl
+
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
+
+# nltk.download()
+
+
+lemmantizer = WordNetLemmatizer()
+
+intents = json.loads(open("intents.json").read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', ',', '.']
+
+for intent in intents['intents']:
+    for pattern in intent["patterns"]:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent["tag"]))
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+
+
+words = [lemmantizer.lemmatize(word)
+         for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open("words.pkl", 'wb'))
+pickle.dump(classes, open("classes.pkl", "wb"))
+
+training = []
+ouput_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmantizer.lemmatize(
+        word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(ouput_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+
+training = np.array(training, dtype=object)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation="softmax"))
+
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss="categorical_crossentropy",
+              optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y),
+                 epochs=200, batch_size=5, verbose=1)
+model.save('chatmodel.h5', hist)
+print("DONE")
